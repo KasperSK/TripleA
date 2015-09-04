a493e25cffc65794962327b52d37210a6fdbf72d
Lrke <lrke.inh@gmail.com>
2015-09-04 14:30:07 +0200

    Latex preample og skabelon

---
+++ Projektdokumentation/Delete_junk.bat
@@ -0,0 +1,16 @@
+echo off
+
+del "*.bbl" /s
+del "*.blg" /s
+del "*.log" /s
+del "*.out" /s
+del "*.toc" /s
+del "*.aux" /s
+del "*.lox" /s
+del "*.tps" /s
+del "*.nlo" /s
+del "*.gz" /s
+del "*.glo" /s
+del "*.glg" /s
+del "*.ist" /s
+del "*.gls" /s
