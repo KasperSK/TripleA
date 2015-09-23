1eee002f72b43ec082a986da227b9a24c4fa95db
Kalle Møller <github.com@k-moeller.dk>
2015-09-23 09:22:04 +0200

    Init project

---
+++ Software/TripleA/CashRegister/Products/IProductDao.cs
@@ -0,0 +1,10 @@
+﻿namespace CashRegister.Products
+{
+    public interface IProductDao
+    {
+        int Create(Product product);
+        Product Read(int pk);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
