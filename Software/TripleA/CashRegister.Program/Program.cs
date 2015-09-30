017214cd264425600717772a320598a9bd3b266f
Kalle Møller <github.com@k-moeller.dk>
2015-09-30 10:55:12 +0200

    Logger oprettet

---
+++ Software/TripleA/CashRegister.Program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CashRegister.Log;
+
+namespace CashRegister.Program
+{
+    class Program
+    {
+        static Program()
+        {
+            LogFactory.Configure();
+        }
+
+        private static ILogger _logger;
+        static void Main(string[] args)
+        {
+            _logger = LogFactory.GetLogger(typeof (Program));
+            _logger.Fatal("Fatal");
+            _logger.Error("Error");
+            _logger.Warn("Warn");
+            _logger.Info("Info");
+            _logger.Debug("Debug");
+        }
+    }
+}
---
+++ Software/TripleA/CashRegister/CashRegister/Log/Logger.cs
@@ -0,0 +1,52 @@
+﻿
+using log4net.Config;
+
+namespace CashRegister.Log
+{
+    using log4net;
+
+    /// <summary>
+    /// Wrapper for log4net
+    /// </summary>
+    public class Logger : ILogger
+	{
+		private readonly ILog _log4Net;
+
+        internal static void Configure()
+        {
+            BasicConfigurator.Configure();
+        }
+        
+		public virtual void Warn(string line)
+		{
+			_log4Net.Warn(line);
+		}
+
+		public virtual void Info(string line)
+		{
+			_log4Net.Info(line);
+		}
+
+		public virtual void Error(string line)
+		{
+			_log4Net.Error(line);
+		}
+
+		public virtual void Fatal(string line)
+		{
+			_log4Net.Fatal(line);
+		}
+
+		public virtual void Debug(string line)
+		{
+			_log4Net.Debug(line);
+		}
+
+		internal Logger(System.Type type)
+		{
+		    _log4Net = LogManager.GetLogger(type);
+		}
+
+	}
+}
+
