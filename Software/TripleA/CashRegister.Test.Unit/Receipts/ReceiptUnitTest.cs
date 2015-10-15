0631383f4256313597e428b7f0038aa27b055440
Kristian Mosegaard <kristian@mosen.me>
2015-10-15 07:53:20 +0200

    Added Receipt namespace.

---
+++ Software/TripleA/CashRegister.Test.Unit/Receipts/ReceiptUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using CashRegister.Receipts;
+using NUnit.Framework;
+
+namespace CashRegister.Test.Unit.Receipts
+{
+    [TestFixture]
+    public class ReceiptUnitTest
+    {
+        [Test]
+        public void Ctor_InitiateRecieptAndTypeOfContentIsListOfString_ContentIsListOfString()
+        {
+            var uut = new Receipt();
+            Assert.That(uut.Content, Is.TypeOf<List<string>>());
+        }
+    }
+}
---
+++ Software/TripleA/CashRegister/CashRegister/Receipts/Receipt.cs
@@ -0,0 +1,22 @@
+﻿using CashRegister.Orders;
+using CashRegister.Printer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CashRegister.Receipts
+{
+	public class Receipt
+	{
+        /// <summary>
+        /// A list of strings with the content of the Receipt
+        /// </summary>
+        public List<string> Content { get; private set; }
+
+	    public Receipt()
+	    {
+	        Content = new List<string>();
+	    }
+	}
+}
