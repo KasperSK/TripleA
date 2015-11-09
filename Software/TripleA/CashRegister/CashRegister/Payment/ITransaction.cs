6c8260462f2efbe3ae2264ce9cbcfc0d5189c1d9
Lrke <lrke.inh@gmail.com>
2015-11-09 09:07:14 +0100

    Print transaktion - kun for cash

--- Software/TripleA/CashRegister/CashRegister/Payment/ITransaction.cs
+++ Software/TripleA/CashRegister/CashRegister/Payment/ITransaction.cs
@@ -2,18 +2,19 @@
 
 namespace CashRegister.Payment
 {
-    public interface ITransaction 
-	{
-		int Amount { get;set; }
+    public interface ITransaction
+    {
+        IPaymentProvidorDescriptor PaymentDescriptor { get; set; }
+        int Amount { get; set; }
 
-		string Description { get;set; }
+        string Description { get; set; }
 
-		int ID { get; }
+        int ID { get; }
 
-		IEnumerable<ITransactionObserver> Observers { get;set; }
+        IEnumerable<ITransactionObserver> Observers { get; set; }
 
-		TransactionStatus Status { get;set; }
+        TransactionStatus Status { get; set; }
 
-		void AddObserver(ITransactionObserver observer);
-	}
+        void AddObserver(ITransactionObserver observer);
+    }
 }
