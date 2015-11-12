f4f53755be241023d5093f8dff31358a1b55696b
Kalle Møller <github.com@k-moeller.dk>
2015-11-12 11:56:38 +0100

    Description added Temp

--- Software/TripleA/CashRegister/CashRegister/Models/Transaction.cs
+++ Software/TripleA/CashRegister/CashRegister/Models/Transaction.cs
@@ -1,10 +1,21 @@
+using CashRegister.Payment;
+
 namespace CashRegister.Models
 {
     using System;
 
+    public enum TransactionStatus
+    {
+        Created = 0,
+        Completed = 1,
+        Failed = 2,
+    }
+
     public class Transaction
     {
         public long Id { get; set; }
+
+        public string Description { get; set; }
 
         public DateTime Date { get; set; }
 
@@ -12,6 +23,8 @@ namespace CashRegister.Models
 
         public virtual SalesOrder SalesOrder { get; set; }
 
-        public virtual PaymentType Paymenttype { get; set; }
+        public virtual IPaymentProvidorDescriptor Paymenttype { get; set; }
+
+        public TransactionStatus Status { get; set; }
     }
 }
