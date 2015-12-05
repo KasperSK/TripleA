6076d221ca9fa04ad9f014c2102703f6fe8a27d4
Kalle Møller <github.com@k-moeller.dk>
2015-12-05 19:50:19 +0100

    Cleanup

--- Software/TripleA/CashRegister.Test.Unit/ViewModels/TabViewUnitTest.cs
+++ Software/TripleA/CashRegister.Test.Unit/ViewModels/TabViewUnitTest.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CashRegister.GUI.ViewModels;
 using CashRegister.Models;
 using CashRegister.Sales;
@@ -15,28 +12,40 @@ namespace CashRegister.Test.Unit.ViewModels
 {
     public class MakeProducts
     {
-
-        private Collection<ProductTab> _testCollection = new Collection<ProductTab>();
+        private readonly Collection<ProductTab> _testCollection = new Collection<ProductTab>();
         public IReadOnlyCollection<ProductTab> TestData => _testCollection;
 
         public void AddProductTab(bool active, string color, int id, string name, int priority)
         {
-            _testCollection.Add(new ProductTab { Active = active, Name = name, Id = id, Color = color, Priority = priority });
+            _testCollection.Add(new ProductTab
+            {
+                Active = active,
+                Name = name,
+                Id = id,
+                Color = color,
+                Priority = priority
+            });
         }
 
         public void AddProductType(string color, int id, int price, string name, int tabId)
         {
-             _testCollection.First(p => p.Id == tabId).ProductTypes.Add(new ProductType {Color = color, Id = id, Name = name, Price = price});
+            _testCollection.First(p => p.Id == tabId)
+                .ProductTypes.Add(new ProductType {Color = color, Id = id, Name = name, Price = price});
         }
 
         public void AddProductGroup(string name, int id, int tabId, int typeId)
         {
-            _testCollection.First(p => p.Id == tabId).ProductTypes.First(p => p.Id == typeId).ProductGroups.Add(new ProductGroup {Id = id, Name = name});
+            _testCollection.First(p => p.Id == tabId)
+                .ProductTypes.First(p => p.Id == typeId)
+                .ProductGroups.Add(new ProductGroup {Id = id, Name = name});
         }
 
         public void AddProduct(string name, int price, bool saleable, int tabId, int typeId, int groupId)
         {
-            _testCollection.First(p => p.Id == tabId).ProductTypes.First(p => p.Id == typeId).ProductGroups.First(p => p.Id == groupId).Products.Add(new Product(name,price,saleable));
+            _testCollection.First(p => p.Id == tabId)
+                .ProductTypes.First(p => p.Id == typeId)
+                .ProductGroups.First(p => p.Id == groupId)
+                .Products.Add(new Product(name, price, saleable));
         }
 
         public Product ReturnProduct(int tabId, int typeId, int groupId, string name)
@@ -55,18 +64,8 @@ namespace CashRegister.Test.Unit.ViewModels
     }
 
     [TestFixture]
-    class TabViewUnitTest
+    internal class TabViewUnitTest
     {
-
-
-        private MakeProducts _testProducts;
-
-        private TabViewModel _uut;
-        private ISalesController _fakeSalesController;
-        private INumpad _fakeNumpad;
-        private INotifyTest _fakeNotifyTest;
-        private bool first = false;
-
         [SetUp]
         public void SetUp()
         {
@@ -81,14 +80,28 @@ namespace CashRegister.Test.Unit.ViewModels
             _testProducts.AddProductType("Red", 1, 20, "RedSnask", 1);
             _testProducts.AddProductGroup("RedHat", 2, 1, 1);
             _testProducts.AddProduct("GreenAle", 20, true, 1, 1, 2);
+        }
 
 
+        private MakeProducts _testProducts;
+
+        private TabViewModel _uut;
+        private ISalesController _fakeSalesController;
+        private INumpad _fakeNumpad;
+        private INotifyTest _fakeNotifyTest;
+
+        [Test]
+        public void AddProduct_ICommand_CallsSalesCtrl()
+        {
+            var command = _uut.AddProduct;
+            command.Execute(new Product("YellowAle", 20, true));
+            _fakeSalesController.Received(1).AddProductToOrder(Arg.Any<Product>(), Arg.Any<int>(), Arg.Any<Discount>());
         }
 
         [Test]
         public void BackgroundColor_SetColor_NotifyCalled()
         {
-            var _uut = new TabViewModel(_fakeSalesController, _fakeNumpad);
+            _uut = new TabViewModel(_fakeSalesController, _fakeNumpad);
             _uut.PropertyChanged += _fakeNotifyTest.TestINotify;
 
             _uut.BackGroundColour = "Green";
@@ -97,13 +110,93 @@ namespace CashRegister.Test.Unit.ViewModels
         }
 
         [Test]
-        public void FetchView_TeabHead_HasRightName()
+        public void ChangeTab_ICommand_SetsNewBgColor()
+        {
+            _testProducts.AddProductTab(true, "Yellow", 2, "YellowStuff", 1);
+            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
+
+            _uut.FetchView();
+
+            var command = _uut.ChangeTab;
+            command.Execute(2);
+
+            Assert.That(_uut.BackGroundColour, Is.EqualTo("Yellow"));
+        }
+
+        [Test]
+        public void FetchView_TabItem_HasRightColor()
         {
             _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
 
             _uut.FetchView();
 
-            Assert.That(_uut.TabHead.First().Name, Is.EqualTo("RedStuff"));
+            Assert.That(_uut.TabItems.First().Colour, Is.EqualTo("Red"));
+        }
+
+        [Test]
+        public void FetchView_TabItem_HasRightColumn()
+        {
+            _testProducts.AddProduct("BlueAle", 20, true, 1, 1, 2);
+            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
+
+            _uut.FetchView();
+
+            Assert.That(_uut.TabItems.First(p => p.Name == "BlueAle").Column, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FetchView_TabItem_HasRightCommand()
+        {
+            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
+
+            _uut.FetchView();
+
+            Assert.That(_uut.TabItems.First().Command, Is.EqualTo(_uut.AddProduct));
+        }
+
+        [Test]
+        public void FetchView_TabItem_HasRightName()
+        {
+            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
+
+            _uut.FetchView();
+
+            Assert.That(_uut.TabItems.First().Name, Is.EqualTo("GreenAle"));
+        }
+
+        [Test]
+        public void FetchView_TabItem_HasRightProduct()
+        {
+            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
+
+            _uut.FetchView();
+
+            Assert.That(_uut.TabItems.First().Product, Is.EqualTo(_testProducts.ReturnProduct(1, 1, 2, "GreenAle")));
+        }
+
+        [Test]
+        public void FetchView_TabItem_HasRightProductType()
+        {
+            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
+
+            _uut.FetchView();
+
+            Assert.That(_uut.TabItems.First().ProductType, Is.EqualTo(_testProducts.RetrunProductType(1, 1)));
+        }
+
+        [Test]
+        public void FetchView_TabItem_HasRightRow()
+        {
+            _testProducts.AddProduct("BlueAle", 20, true, 1, 1, 2);
+            _testProducts.AddProduct("PinkAle", 20, true, 1, 1, 2);
+            _testProducts.AddProduct("BrownAle", 20, true, 1, 1, 2);
+            _testProducts.AddProduct("GrayAle", 20, true, 1, 1, 2);
+            _testProducts.AddProduct("YellowAle", 20, true, 1, 1, 2);
+            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
+
+            _uut.FetchView();
+
+            Assert.That(_uut.TabItems.First(p => p.Name == "YellowAle").Row, Is.EqualTo(1));
         }
 
         [Test]
@@ -137,101 +230,13 @@ namespace CashRegister.Test.Unit.ViewModels
         }
 
         [Test]
-        public void FetchView_TabItem_HasRightColor()
+        public void FetchView_TeabHead_HasRightName()
         {
             _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
 
             _uut.FetchView();
 
-            Assert.That(_uut.TabItems.First().Colour, Is.EqualTo("Red"));
-        }
-
-        [Test]
-        public void FetchView_TabItem_HasRightName()
-        {
-            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
-
-            _uut.FetchView();
-
-            Assert.That(_uut.TabItems.First().Name, Is.EqualTo("GreenAle"));
-        }
-
-        [Test]
-        public void FetchView_TabItem_HasRightCommand()
-        {
-            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
-
-            _uut.FetchView();
-
-            Assert.That(_uut.TabItems.First().Command, Is.EqualTo(_uut.AddProduct));
-        }
-
-        [Test]
-        public void FetchView_TabItem_HasRightProduct()
-        {
-            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
-
-            _uut.FetchView();
-
-            Assert.That(_uut.TabItems.First().Product, Is.EqualTo(_testProducts.ReturnProduct(1,1,2, "GreenAle")));
-        }
-
-        [Test]
-        public void FetchView_TabItem_HasRightProductType()
-        {
-            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
-
-            _uut.FetchView();
-
-            Assert.That(_uut.TabItems.First().ProductType, Is.EqualTo(_testProducts.RetrunProductType(1,1)));
-        }
-
-        [Test]
-        public void FetchView_TabItem_HasRightColumn()
-        {
-            _testProducts.AddProduct("BlueAle",20,true,1,1,2);
-            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
-
-            _uut.FetchView();
-
-            Assert.That(_uut.TabItems.First(p => p.Name == "BlueAle").Column, Is.EqualTo(1));
-        }
-
-        [Test]
-        public void FetchView_TabItem_HasRightRow()
-        {
-            _testProducts.AddProduct("BlueAle", 20, true, 1, 1, 2);
-            _testProducts.AddProduct("PinkAle", 20, true, 1, 1, 2);
-            _testProducts.AddProduct("BrownAle", 20, true, 1, 1, 2);
-            _testProducts.AddProduct("GrayAle", 20, true, 1, 1, 2);
-            _testProducts.AddProduct("YellowAle", 20, true, 1, 1, 2);
-            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
-
-            _uut.FetchView();
-
-            Assert.That(_uut.TabItems.First(p => p.Name == "YellowAle").Row, Is.EqualTo(1));
-        }
-
-        [Test]
-        public void AddProduct_ICommand_CallsSalesCtrl()
-        {
-            var Command = _uut.AddProduct;
-            Command.Execute(new Product("YellowAle", 20, true));
-            _fakeSalesController.Received(1).AddProductToOrder(Arg.Any<Product>(), Arg.Any<int>(), Arg.Any<Discount>());
-        }
-
-        [Test]
-        public void ChangeTab_ICommand_SetsNewBgColor()
-        {
-            _testProducts.AddProductTab(true, "Yellow", 2, "YellowStuff", 1);
-            _fakeSalesController.ProductTabs.Returns(_testProducts.TestData);
-
-            _uut.FetchView();
-
-            var Command = _uut.ChangeTab;
-            Command.Execute(2);
-            
-            Assert.That(_uut.BackGroundColour, Is.EqualTo("Yellow"));
+            Assert.That(_uut.TabHead.First().Name, Is.EqualTo("RedStuff"));
         }
     }
 }
