f683120bdcbb8bd66850f478e87913a74314f69f
Kalle Møller <github.com@k-moeller.dk>
2015-12-08 10:07:34 +0100

    Removing unused

--- Software/TripleA/CashRegister.Db/PaymentType.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[PaymentType]
-(
-    [PaymentTypeId]			 BIGINT IDENTITY NOT NULL,
-    [PaymentTypeDescription] BIGINT NOT NULL,
-CONSTRAINT [pk_PaymentType] PRIMARY KEY CLUSTERED (PaymentTypeId)
-)
-
-GO
